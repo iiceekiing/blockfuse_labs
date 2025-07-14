48b9f133d014ee2bbd0c5c4034ef0ba590ac2c72
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-14 13:06:45 +0100

    Add: simple calculator with fixed numbers

---
+++ version_control/python_practice/basic_cal2.py
@@ -0,0 +1,7 @@
+num1 = input("Enter first number: ")
+
+num2 = input("Enter second number: ")
+
+summation = num1 + num2
+
+print(summation)
---
+++ version_control/python_practice/basic_cal4.py
@@ -0,0 +1,7 @@
+num1 = input("Enter first number: ")
+
+num2 = input("Enter second number: ")
+
+summation = float(num1)  + float(num2)
+
+print(summation)
