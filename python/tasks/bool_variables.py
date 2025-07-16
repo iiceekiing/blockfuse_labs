c1704d1000d56609179d7ba3cada28b5bd244284
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-16 11:50:12 +0100

    update: task 2 added to bool_variables.py

--- python/tasks/bool_variables.py
+++ python/tasks/bool_variables.py
@@ -1,3 +1,8 @@
+# task 1: 
+
+''' declare four variables, and save string values in them such that when you run them, two of the output should be false and the other two should be true.
+'''
+
 #false strings
 
 name  = ""
@@ -14,3 +19,26 @@ print(bool(name))
 print(bool(userName))
 print(bool(num1))
 print(bool(num2))
+
+
+
+# task 2
+
+''' 
+declare four variables, and save integer values in them such that when you run them, 
+two of the output should be false and the other two should be true.
+
+'''
+
+int1 = 0
+
+int2 = -1+1
+
+number = 10
+
+integer = 5
+
+print(bool(int1))
+print(bool(int2))
+print(bool(number))
+print(bool(integer))
