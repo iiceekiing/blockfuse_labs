e0d1c1a41d15430aa054a04b7d3a5e156de6aeef
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-16 16:33:31 +0100

    add: allowance.py

---
+++ python/tasks/allowance.py
@@ -0,0 +1,31 @@
+allowance = 2000
+
+allowance -= 400
+#print(allowance)
+
+allowance += 100 
+#print(allowance)
+
+allowance -= 250
+#print(allowance)
+
+_25_percent = 0.25 * allowance
+
+#print(_25_percent)
+
+allowance  -= _25_percent
+
+#print(allowance)
+
+one_third = 0.333333333
+
+#print(one_third)
+
+allowance *= one_third 
+#print(allowance)
+
+allowance //= 2
+
+print(allowance)
+
+
