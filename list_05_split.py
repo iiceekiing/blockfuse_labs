9b6bca553ad1dc0cddcbcba84e1057193c523505
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-22 20:07:16 +0100

    add: list_05_split.py - Using split() to convert string to list

---
+++ list_05_split.py
@@ -0,0 +1,9 @@
+# Using split() to convert a string into a list
+
+sentence = "I love learning Python"
+print("Original sentence:", sentence)
+
+# Split by space
+words = sentence.split()
+print("Split into list:", words)
+
