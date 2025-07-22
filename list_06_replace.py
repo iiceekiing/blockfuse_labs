7b262b4d8b9c0c8bebee8d6adf95e24af5b12e78
iiceekiing <iiceekiingsocials@gmail.com>
2025-07-22 20:07:39 +0100

    add: list_06_replace.py - Replacing list items by assignment

---
+++ list_06_replace.py
@@ -0,0 +1,9 @@
+# Replacing items in a list (not a method, but via assignment)
+
+cities = ["Lagos", "Abuja", "Kano"]
+print("Original list:", cities)
+
+# Replace index 1
+cities[1] = "Ibadan"
+print("After replacement:", cities)
+
